31270b5797d89ce65e4778f6426546a142bd5288
João Ricardo Simplício Soares <joaoricardosimplicio16@gmail.com>
2021-11-22 20:31:43 -0300

    Change way to get arguments from cli

--- src/crawlers/main.js
+++ src/crawlers/main.js
@@ -2,21 +2,34 @@ const registerCrawledProduct = require('../services/register')
 const getCrawlerStoreRespective = require('../services/toolbox')
 
 
+function parserArguments(args){
+  let arguments = {}
+  args.forEach(arg => {
+    let [option, value] = arg.split('=')
+    if (option == 'crawler' || option == 'action'){
+      arguments[`${option}`] = value
+    }
+  })
+  return arguments
+}
+
 if (process.argv[3]){
   const url = process.argv[2]
-  const nameCrawler = process.argv[3]
-  const action = process.argv[4]
-  const CrawlerStore = getCrawlerStoreRespective(nameCrawler)
-  if (CrawlerStore != '' && url.startsWith('http')){
-    const crawlerStore = new CrawlerStore(url, nameCrawler)
-    crawlerStore.getPageInformation()
-    setTimeout(() => {
-      if (action == 'save'){
-        registerCrawledProduct({ crawledProduct: crawlerStore.info })
-      }
-      else {
-        console.log(crawlerStore.info)
-      }
-    }, 5000)
+  const arguments = parserArguments(process.argv)
+  if (arguments['crawler'] != undefined){
+    const CrawlerStore = getCrawlerStoreRespective(arguments['crawler'])
+    if (CrawlerStore != '' && url.startsWith('http')){
+      const crawlerStore = new CrawlerStore(url, arguments['crawler'])
+      crawlerStore.getPageInformation()
+      setTimeout(() => {
+        if (arguments['action'] == 'save'){
+          registerCrawledProduct({ crawledProduct: crawlerStore.info })
+        }
+        else {
+          console.clear()
+          console.log(crawlerStore.info)
+        }
+      }, 5000)
+    }
   }
 }
